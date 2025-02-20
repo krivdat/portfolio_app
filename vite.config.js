ef9326ea99acdb45c08fcac52746bf6d84dbbabf
Tomas Krivda <krivdat@gmail.com>
2025-02-20 11:14:09 +0100

    changes to allow access from internet

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-	server: {
-
- 		port: 5173,
-
- 		strictPort: false,
-
- 	}
+server: {
+        allowedHosts: ['dev.tomaskrivda.online']
+    }
 });
